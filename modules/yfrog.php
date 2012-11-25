21517868c7603343b2e75f08b01d5bcef03b849c
Sergio Cruz <dev@sergio.cat>
2012-11-25 20:04:33 +0100

    Added support for yfrog videos

--- modules/yfrog.php
+++ modules/yfrog.php
@@ -10,8 +10,15 @@ function loadURL($url) {
 	if ($get[0] === false) {
 		return array(2);
 	}
-	if (!preg_match('#<meta property="og:image" content="(.+)" />#', $get[0], $tmp)) {
+	if (preg_match('#Site.LP.setMode\(\'photo\'\);#', $get[0])) {
+		$pattern = '#<meta property="og:image" content="(.+)" />#';
+		$isImage = true;
+	} else {
+		$pattern = '#<div id="share-links" class=\'pw-widget pw-size-large\' pw:pinterest-image="(.+)">#';
+		$isImage = false;
+	}
+	if (!preg_match($pattern, $get[0], $tmp)) {
 		return array(2);
 	}
-	return array(false, $tmp[1], true);
+	return array(false, $tmp[1], $isImage);
 }
